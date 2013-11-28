7b376793fd86089ceab3cab90d859b825dd616e7
NIghtmechanic <thenightmechanic@gmail.com>
2013-11-28 02:06:16 +0200

    Prototype FW Ver 0
    Display and BMP180 working
    
    To Do:
    1. Altitude Kalman filter
    2. Relative altitude (+zeroing)
    3. Apogee detection
    4. Servo control

---
+++ Firmware/WRFL_Proto/Debug/main.pp
@@ -0,0 +1,59 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: ../Nokia5110.h
+main.obj: ../platform.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_types.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_ints.h
+main.obj: ../wrfl_proto_mux.h
+main.obj: ../gears_logo.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h
+main.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/debug.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/interrupt.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/rom.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/ssi.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/systick.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/sensorlib/hw_bmp180.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/sensorlib/i2cm_drv.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/sensorlib/bmp180.h
+main.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/rom_map.h
+
+../main.c: 
+../Nokia5110.h: 
+../platform.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_types.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_ints.h: 
+../wrfl_proto_mux.h: 
+../gears_logo.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/math.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/ymath.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/debug.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/interrupt.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/rom.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/ssi.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/systick.h: 
+E:/ti/TivaWare_C_Series-1.1/sensorlib/hw_bmp180.h: 
+E:/ti/TivaWare_C_Series-1.1/sensorlib/i2cm_drv.h: 
+E:/ti/TivaWare_C_Series-1.1/sensorlib/bmp180.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/rom_map.h: 
---
+++ Firmware/WRFL_Proto/Debug/wrfl_proto_mux.pp
@@ -0,0 +1,33 @@
+# FIXED
+
+wrfl_proto_mux.obj: ../wrfl_proto_mux.c
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h
+wrfl_proto_mux.obj: E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h
+wrfl_proto_mux.obj: ../wrfl_proto_mux.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_types.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/inc/hw_gpio.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/rom_map.h
+wrfl_proto_mux.obj: E:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h
+
+../wrfl_proto_mux.c: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdint.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdbool.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/yvals.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/stdarg.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/linkage.h: 
+E:/ti/ccsv5/tools/compiler/arm_5.0.4/include/_lock.h: 
+../wrfl_proto_mux.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_types.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_memmap.h: 
+E:/ti/TivaWare_C_Series-1.1/inc/hw_gpio.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/sysctl.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/pin_map.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/rom_map.h: 
+E:/ti/TivaWare_C_Series-1.1/driverlib/gpio.h: 
