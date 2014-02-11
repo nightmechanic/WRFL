b8f2469cd5c35ee93b6a252fb4a02e5d23aa1368
NIghtmechanic <thenightmechanic@gmail.com>
2014-02-12 00:13:36 +0200

    fixed untracked files

--- Firmware/WRFL_Proto/Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -l"E:/ti/TivaWare_C_Series-1.1/driverlib/ccs/Debug/driverlib.lib" -l"libc.a" -l"E:/ARM/CMSIS/dsplib-cm4f/CMSIS_DSP_W_CHECK/dsplib-cm4f.lib"
-
